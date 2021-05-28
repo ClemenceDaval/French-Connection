33cd0faca6fa280c26b76d897c675ddcdc765b24
Clemence <daval.clemence@gmail.com>
2021-05-28 14:03:46 +0200

    renaming of MapProfile to ProfileMap + deletion of useless MapProfile container

--- src/components/ProfileMap/index.js
+++ src/components/ProfileMap/index.js
@@ -1,40 +1,29 @@
 import React from 'react';
-import {
-  GoogleMap,
-  Marker,
-} from '@react-google-maps/api';
+import { GoogleMap, Marker } from '@react-google-maps/api';
 
-
-import  './mapProfile.scss';
+import './profileMap.scss';
 
 import '@reach/combobox/styles.css';
 
-// a voir avce Lucas pour cette const
-const libraries = ['places'];
-
-const MapProfile = ({ latProp, lngProp }) => {
-  const center = {lat: latProp, lng: lngProp}
+const ProfileMap = ({ latProp, lngProp }) => {
+  const center = { lat: latProp, lng: lngProp };
 
   const mapContainerStyle = {
     height: '30vh',
-    //width: '22vw',
   };
+
   const options = {
     disableDefaultUI: true,
     zoomControl: true,
   };
-
-  
 
   const mapRef = React.useRef();
   const onMapLoad = React.useCallback((map) => {
     mapRef.current = map;
   }, []);
 
-  
   return (
     <div className="mapProfile">
-      
       <GoogleMap
         id='map'
         mapContainerStyle={mapContainerStyle}
@@ -43,20 +32,12 @@ const MapProfile = ({ latProp, lngProp }) => {
         options={options}
         onLoad={onMapLoad}
       >
-
-    <Marker
-      position={{ lat: latProp, lng: lngProp }}
-    />
-        
+        <Marker
+          position={{ lat: latProp, lng: lngProp }}
+        />
       </GoogleMap>
     </div>
   );
 };
 
-
-  
-  
-
-
-
-export default MapProfile;
+export default ProfileMap;
